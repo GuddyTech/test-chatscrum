c2a4886ded8bef7700b76c4c382c9c4cd66677bf
Okorie GoodGod <guddytechs@gmail.com>
2022-09-19 16:49:03 +0100

    first sign-up

---
+++ src/components/sign-up/sign-up.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '/.sign-up.css'
+
+const SignUp = () => (
+    <div className="sign-up">
+        <h1> Don't have an account?</h1>
+        <h3> Sign Up here!</h3>
+        <label for="text"> Full Name </label>
+            <input name="text" type="text"/>
+            <label for="email"> Email </label>
+            <input name="text" type="text"/>
+            <label for="password"> Password </label>
+            <input name="text" type="text"/>
+
+
+
+    </div>
+)
