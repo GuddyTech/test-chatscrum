3d06f663662902f743b166d9cf634f01831901db
Okorie GoodGod <guddytechs@gmail.com>
2022-09-19 20:40:32 +0100

    the sign up component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
+import SignUp from  './components/sign-up/sign-up'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1> MY CHATSCRUM </h1>
+        <SignUp />
       </div>
     );
   }
--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
-import '/.sign-up.css'
+import './sign-up.css'
+
 
 const SignUp = () => (
     <div className="sign-up">
         <h1> Don't have an account?</h1>
         <h3> Sign Up here!</h3>
-        <label for="text"> Full Name </label>
-            <input name="text" type="text"/>
-            <label for="email"> Email </label>
-            <input name="text" type="text"/>
-            <label for="password"> Password </label>
-            <input name="text" type="text"/>
+        <form>
+            <label for="text"> Full Name </label>
+                <input name="text" type="text"/>
+                <label for="email"> Email </label>
+                <input name="email" type="email"/>
+                <label for="password"> Password </label>
+                <input name="password" type="password"/>
+            
+            <label for="options"> User Type </label>
+            <select id="options">
+                <option value="Developer"> Developer </option>
+                <option value="Owner"> Owner </option>
+                <option value="Client"> Client </option>
+            </select>
 
-
-
+            <button> SIGN UP </button>
+        </form>
     </div>
 )
+
+export default SignUp;
